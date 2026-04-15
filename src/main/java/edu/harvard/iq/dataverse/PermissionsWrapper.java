f1ebfafe54d649282e6dd7f5be233cffaab8806a
Steven Winship <39765413+stevenwinship@users.noreply.github.com>
2026-04-15 15:00:21 -0400

    Inconsistent behaviour of dataset delete API

--- src/main/java/edu/harvard/iq/dataverse/PermissionsWrapper.java
+++ src/main/java/edu/harvard/iq/dataverse/PermissionsWrapper.java
@@ -249,7 +249,7 @@ public class PermissionsWrapper implements java.io.Serializable {
 
     // DELETE DATASET
     public boolean canIssueDeleteDatasetCommand(DvObject dvo){
-        return canIssueCommand(dvo, DeleteDatasetCommand.class);
+        return canIssueCommand(dvo, DeleteDatasetVersionCommand.class);
     }
     
     // PUBLISH DATASET
